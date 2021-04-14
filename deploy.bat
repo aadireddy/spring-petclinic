5862f04ef1a9e3f2200686420dca5f600e7d94e0
Karthik Reddy <aadi.karthik77@gmail.com>
2021-04-14 09:20:04 +0530

    Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 REM delete existing file
-del "C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\online1\target\*.war"
+del "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\*.jar"
 
 REM source folder location
-set Source_Folder=C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\online1\target\*.war
+set Source_Folder=C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\online1\target\*.jar
 REM destination folder location
 set Dest_Folder=C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\
